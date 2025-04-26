55bfe22cf9d14712287b2c65fe7991d8e5ef7fc6
Cristina Salazar <80979107+a-poco@users.noreply.github.com>
2025-04-26 18:42:57 +0200

    added format dates and countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { format, parse } from 'date-fns';
+import { differenceInSeconds, formatDate } from 'date-fns';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,17 +17,61 @@ export class AppComponent {
   date = '';
   inputDate = '';
   showDate = false;
+  now = new Date();
+  targetDate: Date | null = null;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.updateDuration();
+    }, 1000);
+  }
+
   onEnter(event: Event) {
     if ((event as KeyboardEvent).key === 'Enter') {
       if (this.inputTitle === '' || this.inputDate === '') {
         return;
       }
-      this.title = this.inputTitle;
-      this.showTitle = true;
-      this.date = this.inputDate;
-      this.showDate = true;
-      this.inputTitle = '';
-      this.inputDate = '';
+      const parsedDate = parse(this.inputDate, 'yyyyMMdd', new Date());
+      const formattedDate = format(parsedDate, 'yyyy-MM-dd');
+
+      try {
+        this.targetDate = parse(this.inputDate, 'yyyyMMdd', new Date());
+
+        if (this.isValidDate(this.targetDate)) {
+          this.title = this.inputTitle;
+          this.showTitle = true;
+          this.updateDuration();
+          this.showDate = true;
+          this.inputDate = formattedDate;
+        } else {
+          throw new Error('Invalid date');
+        }
+      } catch (error) {
+        alert(
+          'Please enter a valid date in YYYYMMDD format (e.g., 20241225 for December 25, 2024)'
+        );
+        this.inputDate = '';
+      }
+    }
+  }
+
+  private isValidDate(date: Date): boolean {
+    return date instanceof Date;
+  }
+
+  private updateDuration() {
+    if (this.targetDate) {
+      const now = new Date();
+      let totalSeconds = differenceInSeconds(this.targetDate, now);
+
+      const days = Math.floor(totalSeconds / (3600 * 24));
+      totalSeconds %= 3600 * 24;
+      const hours = Math.floor(totalSeconds / 3600);
+      totalSeconds %= 3600;
+      const minutes = Math.floor(totalSeconds / 60);
+      const seconds = totalSeconds % 60;
+
+      this.date = `${days}days, ${hours}h, ${minutes}m, ${seconds}s`;
     }
   }
 }
